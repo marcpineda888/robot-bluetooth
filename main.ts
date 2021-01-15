47a697f62326b9fe4fad7c493c56c9cb6e9c3902
marcpineda888 <77488025+marcpineda888@users.noreply.github.com>
2021-01-15 11:27:36 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+    basic.pause(1000)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.SemiColon), function () {
+    commande = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (commande == "Avant gauche") {
+        robotbit.MotorRunDelay(robotbit.Motors.M1A, 255, 0.1)
+    } else if (commande == "Avant droit") {
+        robotbit.MotorRunDelay(robotbit.Motors.M1B, 255, 0.1)
+    } else if (commande == "Arrière gauche") {
+        robotbit.MotorRunDelay(robotbit.Motors.M2A, 255, 0.1)
+    } else {
+        robotbit.MotorRunDelay(robotbit.Motors.M2B, 255, 0.1)
+    }
+})
+let commande = ""
+bluetooth.startUartService()
+basic.showString("go")
+basic.pause(1000)
